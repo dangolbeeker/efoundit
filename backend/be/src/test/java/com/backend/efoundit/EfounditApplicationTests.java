1789b1159439e9c82cd7c3cf4a38cabd3f740e72
scottbeekercodes <scottbeekercodes@penguin>
2022-01-02 13:45:52 -0600

    spring init

---
+++ backend/be/src/test/java/com/backend/efoundit/EfounditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.efoundit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfounditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
